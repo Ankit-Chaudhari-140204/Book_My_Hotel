48bc959d205b9b752adbca085a4a3c0aae77d97b
AnkitChaudhari123 <ankit1402200@gmail.com>
2025-08-07 22:40:53 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: {
+            type: String,
+            default: "default-image"
+        },
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1750959220595-778dd7080c52?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
